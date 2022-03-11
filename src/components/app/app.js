aede21ee042e3cd587f74129e9dd833b42f8e0ec
Artem Vlasiuk <artemvlasiuk93@gmail.com>
2022-03-11 14:52:09 +0200

    added props

---
+++ src/components/app/app.js
@@ -0,0 +1,33 @@
+import AppFilter from '../app-filter/app-filter';
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+    const data = [
+        {name: 'John C.', salary: 800, increase: false},
+        {name: 'Alex M.', salary: 3000, increase: true},
+        {name: 'Carl W.', salary: 5000, increase: false}
+    ];
+    return (
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-panel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+            <div>
+                <EmployeesList data={data}/>
+            </div>
+            <div>
+                <EmployeesAddForm/>
+            </div>
+        </div>
+    );
+}
+
+export default App;
